23bb72e1416ba0e621dea47b9bc716680fb7af5e
Emre Güler <21120205303@ismu.edu.tr>
2023-06-11 22:58:53 +0300

    Update w10-main.c
    exit kullanırsak bütün processler durur. pthread_join kullandığımız için o garanti altına alınır. pthread sonlanınca program sonlanır.

--- w10-main.c
+++ w10-main.c
@@ -3,15 +3,16 @@
 
 void *goldcount(void *args){
     long order = (long) args;
-    printf("Hello from thread order-id: %ld-%ld\n", order, pthread_self());
+    printf("id: %ld- Hello from thread order-id: %ld\n", order, pthread_self());
 
-    pthread_exit(NULL);
+    exit(0);
 }
 
-int main(int argc, char *argv){
+int main(int argc, char *argv[]){
     pthread_t threads[5] = {0};
     pthread_create(&threads[0], NULL, goldcount, (void *)0);
 
     pthread_join(threads[0], NULL);
+    printf("main: hello\n");
     return 0;
 }
